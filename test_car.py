65c7b570079ef139476b89b7bcff2228302fa692
gurukiran29506 <gurukiranhippargi@gmail.com>
2026-01-09 10:44:23 +0530

    Add tests for get_service_category function

--- test_car.py
+++ test_car.py
@@ -1,18 +1,30 @@
-from car import display_summary
+from car import display_summary, get_service_category
+
+def test_get_service_category():
+    assert get_service_category(25000) == "Premium Service"
+    assert get_service_category(17000) == "Gold Service"
+    assert get_service_category(12000) == "Silver Service"
+    assert get_service_category(8000) == "Basic Service"
+    assert get_service_category(3000) == "Economy Service"
+
 
 def test_display_summary():
-    owner_name = "john"
-    car_model = "E173"
-    car_type = "Sedan"
-    total_cost = 9505
-    service_category = "Premium Service"
     expected_output = (
         "\n--- Service Summary ---\n"
         "Car Owner Name      : john\n"
         "Car Model           : E173\n"
         "Car Type            : Sedan\n"
         "Total Service Cost  : ₹9505.00\n"
-        "Service Category    : Premium Service"
-        "Year of Production: 2009"
+        "Service Category    : Premium Service\n"
+        "Year of Production  : 2009"
     )
-    assert display_summary(owner_name, car_model, car_type, total_cost, service_category) == expected_output
+
+    result = display_summary(
+        "john",
+        "E173",
+        "Sedan",
+        9505,
+        "Premium Service"
+    )
+
+    assert result == expected_output
